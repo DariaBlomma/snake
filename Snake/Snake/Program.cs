4c43693c0923700582368a457c78ec01580e8c77
daria <kozina.flowers@yandex.ru>
2021-02-14 00:02:50 +0300

    Lesson 8. Vertical line. Figure

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,18 +10,23 @@ namespace Snake
     {
 		static void Main(string[] args)
 		{
+			Console.SetBufferSize(80, 25);
 
-			Point p1 = new Point(1, 3, '*');
-			p1.Draw();
+			//Отрисовка рамочки
+			HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+			HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+			VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+			VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+			upLine.Drow();
+			downLine.Drow();
+			leftLine.Drow();
+			rightLine.Drow();
 
-			Point p2 = new Point(4, 5, '#');
-			p2.Draw();
+			//Отрисовка точек
+			Point p = new Point(4, 5, '*');
+			p.Draw();
 
-			HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-			line.Drow();
-
-			VerticalLine Vline = new VerticalLine(9, 16, 3, '-');
-			Vline.VerticalDraw();
+		
 
 			Console.ReadLine();
 		}
