0e2baec1dbd02014e7c769c85c95d2e14fd49155
daria <kozina.flowers@yandex.ru>
2021-02-15 22:53:57 +0300

    Lesson 13. Polymorphism

--- Snake/Snake/HorizontalLine.cs
+++ Snake/Snake/HorizontalLine.cs
@@ -11,11 +11,25 @@ namespace Snake
         public HorizontalLine(int xLeft, int xRight, int y, char sym)
         {
             pList = new List<Point>();
-           for(int x = xLeft; x <= xRight; x++)
+            for (int x = xLeft; x <= xRight; x++)
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
             }
         }
+           public override void Draw()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            base.Draw();
+            /*foreach (Point p in pList)
+              {
+                p.Draw();
+               }
+             */
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
     }
+    
 }
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,24 +11,44 @@ namespace Snake
     {
 		static void Main(string[] args)
 		{
-			Console.SetBufferSize(80, 25);
-
+			//Console.SetBufferSize(80, 25);
+			VerticalLine vl = new VerticalLine(0, 10, 5, '%');
+			Draw(vl);
 			//Отрисовка рамочки
-			HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+			/*HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
 			HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
 			VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
 			VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
 			upLine.Draw();
 			downLine.Draw();
 			leftLine.Draw();
-			rightLine.Draw();
+			rightLine.Draw();*/
 
 			//Отрисовка точек
 			Point p = new Point(4, 5, '*');
-			Snake snake = new Snake(p, 4, Direction.RIGHT);
-			snake.Draw();
+			/*Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();*/
+			Figure fSnake = new Snake(p, 4, Direction.RIGHT);
+			Draw(fSnake);
+			Snake snake = (Snake)fSnake;
 
-			FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+			HorizontalLine hl = new HorizontalLine(0, 5, 6, '&');
+
+			List<Figure> figures = new List<Figure>();
+			figures.Add(fSnake);
+			figures.Add(vl);
+			figures.Add(hl);
+
+			foreach (var f in figures)
+			{
+				f.Draw();
+			}
+		}
+		static void Draw(Figure figure)
+		{
+			figure.Draw();
+		}
+			/*FoodCreator foodCreator = new FoodCreator(80, 25, '$');
 			Point food = foodCreator.CreateFood();
 			food.Draw();
 			while (true)
@@ -52,8 +72,8 @@ namespace Snake
 					snake.HandleKey(key.Key);
 				}
 
-			}
-		}
+			}*/
+		
 
 	}
 
